75fb1cafb7aa6f6c25a9bfe774e3c803850218ea
Marek Dudek <marek.dudek@gmail.com>
2018-11-30 21:12:58 +0100

    Simple PostgreSQL demo

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,24 @@
 module Main where
 
 import Web.Scotty
+import Database.PostgreSQL.Simple
 import Data.Monoid (mconcat)
 
 import Lib
 
 main :: IO ()
-main = scotty 8080 $
+main = do
+  i <- hello
+  print i
+
+startWebServer :: IO ()
+startWebServer = scotty 8080 $
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+hello :: IO Int
+hello = do
+  conn <- connectPostgreSQL "postgresql://marek@localhost:5432/aws-demo"
+  [Only i] <- query_ conn "select 2 + 2"
+  return i
