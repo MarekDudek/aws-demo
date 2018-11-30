e4e3c707eaa6aa301ab625dad48cfb8b7c71b823
Marek Dudek <marek.dudek@gmail.com>
2018-11-30 20:30:50 +0100

    Scotty example implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Web.Scotty
+import Data.Monoid (mconcat)
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = scotty 8080 $
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
