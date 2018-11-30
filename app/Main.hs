a3aad981c70eb6c6e622193be97a592c8d7251ba
Marek Dudek <marek.dudek@gmail.com>
2018-11-30 23:37:22 +0100

    idiomatic creation of connections and of executing queries

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Web.Scotty
 import Database.PostgreSQL.Simple
 import Data.Monoid (mconcat)
+import Data.Word
 
 import Lib
 
@@ -19,8 +20,24 @@ startWebServer = scotty 8080 $
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
 
+connectionString = "postgresql://aws-demo-user:aws-demo-password@localhost:5432/aws-demo"
+
 hello :: IO Int
 hello = do
-  conn <- connectPostgreSQL "postgresql://aws-demo-user:aws-demo-password@localhost:5432/aws-demo"
+  conn <- connectPostgreSQL connectionString
   [Only i] <- query_ conn "select 2 + 2"
   return i
+
+inMonadicStyle = do
+  conn <- connection
+  putStrLn "2 + 2"
+  mapM_ print =<< ( query_ conn "select 2 + 3" :: IO [Only Int] )
+  
+connection :: IO Connection
+connection = connect defaultConnectInfo {
+    connectDatabase = "aws-demo",
+    connectUser     = "aws-demo-user",
+    connectPassword = "aws-demo-password",
+    connectHost = "localhost",
+    connectPort = 5432 :: Word16
+  }
