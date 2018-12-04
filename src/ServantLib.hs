67417da312776273afde89fdb5446485a5277607
Marek Dudek <marek.dudek@gmail.com>
2018-12-04 13:26:31 +0100

    examples of caputre, query param and request body

--- src/ServantLib.hs
+++ src/ServantLib.hs
@@ -130,3 +130,75 @@ app2 = serve userAPI15 server2
 
 runMain2 :: IO ()
 runMain2 =  run 8080 app2
+
+
+data Position = Position 
+  { xCoord :: Int 
+  , yCoord :: Int
+  } deriving Generic
+
+instance ToJSON Position
+
+newtype HelloMessage = HelloMessage { msg :: String } deriving Generic
+
+instance ToJSON HelloMessage
+
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
+
+instance ToJSON ClientInfo
+instance FromJSON ClientInfo
+
+data Email = Email 
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving Generic
+
+instance ToJSON Email
+
+emailForClient :: ClientInfo -> Email
+emailForClient c = Email from' to' subject' body'
+  where from' = "great@company.com"
+        to' = clientEmail c
+        subject' = "Hey " ++ name ++ ", we miss you!"
+        body' = "Hi " ++ name ++ ", \n\n" 
+            ++ " Since you've recently turned " ++ show (clientAge c)
+            ++ ", have you checked our latest "
+            ++ Data.List.intercalate ", " (clientInterestedIn c)
+            ++ " products? Give us a visit!"
+        name = clientName c
+
+type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
+
+
+server3 :: Server API
+server3 = position
+     :<|> hello
+     :<|> marketing 
+  where position :: Int -> Int -> Handler Position
+        position x y = return (Position x y)
+
+        hello :: Maybe String -> Handler HelloMessage
+        hello mname = return . HelloMessage $ case mname of
+          Nothing -> "Hello, anonymous coward"
+          Just n  -> "Hello, " ++ n
+        
+        marketing :: ClientInfo -> Handler Email
+        marketing ci = return (emailForClient ci)
+
+api :: Proxy API
+api = Proxy
+
+app3 :: Application
+app3 = serve api server3
+
+runMain3 :: IO ()
+runMain3 = run 8080 app3
